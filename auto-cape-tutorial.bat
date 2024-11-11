cd324947e987e1b059ef39d5e98cd5ebbd282c8e
AnakamaTheHedgehog <109427414+Tminec@users.noreply.github.com>
2024-11-11 16:05:36 +0300

    Create auto-cape-tutorial.bat

---
+++ auto-cape-tutorial.bat
@@ -0,0 +1,19 @@
+@echo off
+title Faster Quality - Cape Feature
+color 1F
+echo Faster Quality capes tutorial - AnakamaTheHedgehog
+echo Downloading the png file...
+start https://download1640.mediafire.com/0cduuvzqsq1gncdSqG2Jm9Div2zIbtCNIFqkUelSLDfLoHNQyVYDudBBv_Ff6ZVI5sToH7B7QkhaZLhWR0W-GSFPPFxKO3NkI-kTyFyGHrrUDdeNC0V3jG_4L86W9xbJWqeX1PIkGnTcWHNXAflj1Clai7tgVUfR78d1OiGk0ZNsiPo/qki51fycgmuxnpi/fqcape.png
+timeout 3 > NUL
+echo Press a button after logging in to MinecraftCapes!
+timeout 3 > NUL
+start https://minecraftcapes.net/account/login/minecraft
+pause>nul
+timeout 5 > NUL
+echo Uplaod the fqcape.png file from your downloads folder to the website and enjoy your new cape!
+timeout 5 > NUL
+start https://minecraftcapes.net/upload-cape
+echo Press a button to continue..
+pause>NUL
+echo Press any button to close...
+pause>nul
